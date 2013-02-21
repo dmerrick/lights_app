ff6f8c29231d7179ebd5b44b77f3250a2c68dc0e
Dana Merrick <dmerrick@brightcove.com>
2013-02-20 19:50:49 -0500

    Misc. improvements.

--- lights_app.rb
+++ lights_app.rb
@@ -18,7 +18,7 @@ class LightsApp
     json_body = {:username => @key, :devicetype => @name}.to_json
     response = HTTParty.post(@api_endpoint, :body => json_body)
 
-    if response.first["error"]
+    if response.first["error"] # should be response.code
       puts "Press link button and try again."
       exit
     else
@@ -28,12 +28,20 @@ class LightsApp
 
   # query all lights
   def overview
-    HTTParty.get("#{@api_endpoint}/#{@key}")
+    request_uri = "#{@api_endpoint}/#{@key}"
+    HTTParty.get(request_uri)
   end
 
   # query a single light
   def status(light_id)
-    HTTParty.get("#{@api_endpoint}/#{@key}/lights/#{light_id}")
+    request_uri = "#{@api_endpoint}/#{@key}/lights/#{light_id}"
+    HTTParty.get(request_uri)
+  end
+
+  def set_light(light_id, options)
+    json_body = options.to_json
+    request_uri = "#{@api_endpoint}/#{@key}/lights/#{light_id}/state"
+    HTTParty.put(request_uri, :body => json_body)
   end
 
 end
@@ -42,7 +50,7 @@ if $0 == __FILE__
   # put your customizations here
   app = LightsApp.new("lightsapp", "192.168.1.14")
 
-  # I've commented this out because I already registered
+  # register the script with the Hue
   #app.register!
 
   # human-readable ids
@@ -50,16 +58,35 @@ if $0 == __FILE__
   back_right  = 2
   front_left  = 3
 
-  require 'awesome_print'
-  ap app.overview
+  #require 'awesome_print'
+  #ap app.overview
   #ap app.status front_left
+
+  # flash the lights red and green
+  while true
+    app.set_light(front_left,  :hue => 25000, :sat => 254)
+    app.set_light(front_right, :hue => 25000, :sat => 254)
+    app.set_light(back_right,  :ct  => 500,   :bri => 254)
+    sleep 1
+    app.set_light(front_left,  :ct  => 500,   :bri => 254)
+    app.set_light(front_right, :ct  => 500,   :bri => 254)
+    app.set_light(back_right,  :hue => 25000, :sat => 254)
+    sleep 1
+  end
+
 end
 
 
 __END__
-# register worked:
+# api_url can be found at:
+# https://www.meethue.com/en-US/user/preferencessmartbridge
+
+# extra details about hacking Hues:
+# http://rsmck.co.uk/hue
+
+# register worked response:
 # => [{"success"=>{"username"=>"5a90e47a2ac90131ace46cce377bdc64"}}]
 
-# register failed (press link button):
+# register failed response (press link button):
 # => [{"error"=>
 #     {"address"=>"", "type"=>101, "description"=>"link button not pressed"}}]
