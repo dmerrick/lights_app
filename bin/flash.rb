a888f0a8471dff9fc7cf99ed8a18d1462d333d00
Dana Merrick <dmerrick@brightcove.com>
2013-02-25 21:27:05 -0500

    Adding red/blue flash support.

--- bin/flash.rb
+++ bin/flash.rb
@@ -9,11 +9,13 @@ require 'pry'
 require_relative '../philips_hue'
 
 # set default values here
+colors = {"red" => [0.6446, 0.3289], "blue" => [0.1727, 0.0512]}
+
 options = OpenStruct.new
 options.light_id = 1
-#options.color    = "red"
+options.color    = colors["red"]
 options.delay    = 1
-options.number   = 1
+options.repeat   = 1
 
 OptionParser.new do |opts|
   opts.banner = "Usage: flash.rb [options]"
@@ -22,26 +24,23 @@ OptionParser.new do |opts|
     options.light_id = l
   end
 
-  #opts.on("-c", "--color", "Run verbosely") do |c|
-  #  options.color = c
-  #end
+  opts.on("-c [color]", "The color to flash") do |c|
+    options.color = colors[c]
+  end
 
   opts.on("-t [secs]", Float, "Length of flashes in seconds") do |t|
     options.delay = t
   end
 
-  opts.on("-n [number]", Integer, "Flash [number] times") do |n|
-    options.number = n
+  opts.on("-n [number]", Integer, "Repeat [number] times") do |n|
+    options.repeat = n
   end
 end.parse!
 
 hue = PhilipsHue.new("lightsapp", "192.168.1.14")
 light = hue.light(options.light_id)
-old_xy = light.xy
 
-options.number.times do
+options.repeat.times do
   # flash!
-  light.red
-  sleep options.delay
-  light.xy = old_xy
+  light.flash(options.color, options.delay)
 end
