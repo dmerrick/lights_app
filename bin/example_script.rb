afd9c7258ee53bcc1ee840e2a57ab794e4d279bb
Dana Merrick <dmerrick@brightcove.com>
2013-02-26 12:42:13 -0500

    Updating the example script.

--- bin/example_script.rb
+++ bin/example_script.rb
@@ -1,44 +1,37 @@
 #!/usr/bin/env ruby
-require 'awesome_print'
-
-# requires ruby 1.9 or greater
+# this is a basic example of what you can do with this library
 require_relative '../philips_hue'
 
-# put your customizations here
+# set up your hue app here
 hue = PhilipsHue.new("lightsapp", "192.168.1.14")
-#hue.register!
 
-# assign in order by their light_id
-front_right = hue.add_light(:front_right)
-back_right  = hue.add_light(:back_right)
-front_left  = hue.add_light(:front_left)
+# assign each light to a variable
+light1, light2, light3 = hue.lights
 
-#ap hue.overview
-#ap front_right.state
-#puts front_right.colormode
-#puts front_right.xy
-#front_right.xy = [0.6446, 0.3289]
-puts front_right
+# print status of light1
+puts light1
+
+# true if we want to have a colorful party
+looping = true
 
 # loop pretty colors
-looping = true
 loop do
 
-  front_left.red
-  front_right.green
-  back_right.blue
+  light1.red
+  light2.green
+  light3.blue
 
   sleep 1
 
-  front_left.blue
-  front_right.blue
-  back_right.yellow
+  light1.blue
+  light2.blue
+  light3.yellow
 
   sleep 1
 
-  front_left.yellow
-  front_right.red
-  back_right.red
+  light1.yellow
+  light2.red
+  light3.red
 
   sleep 1
 
