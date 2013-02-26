ec1b2e6bdeb0034c657f83d8ae55c60ebdb20921
Dana Merrick <dmerrick@brightcove.com>
2013-02-25 21:02:31 -0500

    Adding a (super helpful) sandbox tool.

---
+++ bin/sandbox.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require 'pp'
+require 'awesome_print'
+require 'pry'
+require_relative '../philips_hue'
+
+hue = PhilipsHue.new("lightsapp", "192.168.1.14")
+a, b, c = hue.lights
+
+binding.pry
