adf82cffe0a55b734acf3a0324dfd5a75892cd6c
Damien Storm <ryderstorm@users.noreply.github.com>
2015-01-23 08:51:13 -0500

    adding default scenes from hue app

--- lib/philips_hue/helpers.rb
+++ lib/philips_hue/helpers.rb
@@ -44,6 +44,27 @@ module PhilipsHue::Helpers
     self.xy = [0.3127301082804434, 0.3290198826715099]
   end
 
+  # default scenes from Hue app
+  def reading                  
+    self.xy = [0.4448, 0.4066] 
+    self.bri = 240             
+  end                          
+
+  def relax                    
+    self.xy = [0.5119, 0.4147] 
+    self.bri = 144             
+  end                          
+
+  def energize                 
+    self.xy = [0.3151, 0.3252] 
+    self.bri = 203             
+  end                          
+
+  def concentrate              
+    self.xy = [0.368, 0.3686]  
+    self.bri = 219             
+  end
+  
   # flash once
   def blip
     self.alert = "select"
